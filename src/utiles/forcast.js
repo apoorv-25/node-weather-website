7a3021f889dcf186b2547370d5cb2aa12b6cface
apoorv saxena <apoorv saxena@LAPTOP-C79HQM5L>
2020-09-04 00:14:00 +0530

    add humidity

--- src/utiles/forcast.js
+++ src/utiles/forcast.js
@@ -4,13 +4,13 @@ const forcast =(latitude,longitude,callback)=>{
 
  request({url,json:true },(error,{body})=>{
      if(error){
-         callback('unable to coone t',undefined)
+         callback('unable to coonect',undefined)
 
      }else if(body.error){
-         callback('unable to fond',undefined)
+         callback('unable to find',undefined)
 
      }else{
-         callback(undefined,body.current.weather_descriptions[0] +'. it is currently ' + body.current.temperature + ' degrees out. there is ' + body.current.feelslike +' degrees out.')
+        callback(undefined,body.current.weather_descriptions[0] +'. it is currently ' + body.current.temperature + ' degrees out. there is ' + body.current.feelslike +' degrees out.The humidity is '+ body.current.humidity +' %.')
 
      }
  })
